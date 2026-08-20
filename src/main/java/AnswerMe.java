1aad232bf7d623df542f9d394d6b6cfc23e1f80c
RyanNgCT <ryanngct.dev@gmail.com>
2026-08-20 23:04:26 +0800

    AnswerMe class: Add echo and exit functionality

--- src/main/java/AnswerMe.java
+++ src/main/java/AnswerMe.java
@@ -1,10 +1,12 @@
-/**
- * Entry point for the AnswerMe chatbot.
- */
+// imports
+import java.util.Scanner;
+
 public class AnswerMe {
+    public static final String BOTNAME = "AnswerMe";
+    public static final String HLINE = "____________________________________________________________";
+    private static final Scanner SCANNER = new Scanner(System.in);
+
     public static void main(String[] args) {
-        String botName = "AnswerMe";
-        String hLine = "____________________________________________________________";
         String banner = """
                       >>                                                        >=>       >=>          \s
                      >>=>                                                       >> >=>   >>=>          \s
@@ -15,9 +17,35 @@ public class AnswerMe {
                 >=>        >=> >==>  >=> >=> >=> >==>    >==>  >====>   >==>    >=>       >=>  >====>  \s
                 """;
         System.out.println(banner);
-        System.out.println("Hello! I'm " + botName + ", your personal assistant bot.\nWhat can I do for you today?\n");
-        System.out.println(hLine);
-        System.out.println("Bye. Hope to see you again soon!");
-        System.out.println(hLine);
+        System.out.println("Hello! I'm " + BOTNAME + ", your personal assistant bot.\nWhat can I do for you today?\n");
+        System.out.println(AnswerMe.HLINE);
+        System.out.println("What can I do for you today?");
+        System.out.println(AnswerMe.HLINE);
+
+        boolean exitProgram = false;
+        do {
+            String userResponse = readUserInput();
+            exitProgram = echo(userResponse);
+        }
+        while (!exitProgram);
+
+        System.out.println("\nBye. Hope to see you again soon!");
+        System.out.println(AnswerMe.HLINE);
+    }
+
+    public static String readUserInput() {
+        return SCANNER.nextLine();
+    }
+
+    public static boolean echo(String userResponse) {
+        if (userResponse.equalsIgnoreCase("bye")) {
+            return true;
+        }
+        else {
+            System.out.println("\t" + AnswerMe.HLINE);
+            System.out.println("\t" + userResponse);
+            System.out.println("\t" + AnswerMe.HLINE);
+            return false;
+        }
     }
 }
