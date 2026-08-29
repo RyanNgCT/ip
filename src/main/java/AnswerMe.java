3d3ed0a251ae6113316d345a66e61f6c676e297a
RyanNgCT <ryanngct.dev@gmail.com>
2026-08-29 16:34:02 +0800

    Refactor AnswerMe class

--- src/main/java/AnswerMe.java
+++ src/main/java/AnswerMe.java
@@ -1,151 +1,36 @@
-// imports
-import java.time.LocalDateTime;
-
-import java.util.Arrays;
-import java.util.HashMap;
-
 public class AnswerMe {
-    private static TaskList taskList = new TaskList();
+    private Storage storage;
+    private TaskList taskList;
+    private Ui ui;
 
-    public static void main(String[] args) {
-        Storage storage = new Storage();
-        AnswerMe.taskList = storage.loadTasks();
-        Ui ui = new Ui();
+    public AnswerMe() {
+        storage = new Storage();
+        ui = new Ui();
+        try {
+            taskList = new TaskList(storage.loadTasks());
+        } catch (AnswerMeException e) {
+            ui.showLoadingError();
+            taskList = new TaskList();
+        }
+    }
+
+    public void run() {
         ui.showWelcome();
-        boolean exitProgram = false;
-        do {
-            String userResponse = ui.readUserInput();
+        boolean isExit = false;
+        while (!isExit) {
             try {
-                exitProgram = process(userResponse, ui, storage);
-            }
-            catch(AnswerMeException e) {
+                String input = ui.readUserInput();
+                Command cmd = Parser.parse(input);
+                cmd.execute(taskList, ui, storage);
+                isExit = cmd.isExit();
+            } catch (AnswerMeException e) {
                 ui.showMessage(e.getMessage());
             }
         }
-        while (!exitProgram);
-        ui.showMessage("Bye. Hope to see you again soon!");
     }
 
-
-    public static boolean process(String userResponse, Ui ui, Storage storage)
-            throws AnswerMeException {
-        String[] responseParts = userResponse.split(" ");
-
-        switch (responseParts[0].toLowerCase()){
-            case "bye":
-                return true;
-
-            case "list":
-                new ListCommand().execute(AnswerMe.taskList, ui, storage);
-                break;
-
-            case "mark":
-            case "unmark":
-            case "delete":
-                String command = responseParts[0];
-                if (taskList.isEmpty()) {
-                    throw new AnswerMeException("The list is empty so we have nothing to " + command + ".");
-                }
-                int index = extractListIndex(responseParts);
-
-                // set, unset or delete based on first arg
-                if (command.equals("mark")) {
-                   new MarkCommand(index).execute(AnswerMe.taskList, ui, storage);
-                }
-                else if (command.equals("unmark")) {
-                    new UnmarkCommand(index).execute(AnswerMe.taskList, ui, storage);
-                }
-                else {
-                    new DeleteCommand(index).execute(AnswerMe.taskList, ui, storage);
-                }
-                break;
-
-            default:
-                try {
-                    addTask(userResponse, ui, storage);
-                }
-                catch (AnswerMeException e) {
-                    ui.showMessage(e.getMessage());
-                }
-                break;
-        }
-        return false;
-    }
-
-    public static int extractListIndex(String[] responseParts) throws AnswerMeException {
-        if (responseParts == null || responseParts.length < 2) {
-            throw new AnswerMeException("An index must be supplied for this command.");
-        }
-        int index;
-        try {
-            index = Integer.parseInt(responseParts[1]);
-        } catch (NumberFormatException ex) {
-            throw new AnswerMeException("The list index must be a valid integer.");
-        }
-
-        if (index < 1 || index > AnswerMe.taskList.size()) {
-            throw new AnswerMeException("The task does not exist in the list.");
-        }
-        return index - 1;
-    }
-
-    public static void addTask(String userResponse, Ui ui, Storage storage) throws AnswerMeException{
-        String[] responseParts = userResponse.split(" ");
-        String command = responseParts[0].toLowerCase();
-        String args = extractArgs(responseParts);
-
-        switch (command) {
-            case "todo":
-                if (args.isEmpty()) {
-                    throw new AnswerMeException("Format: todo <description>");
-                }
-                new AddToDoCommand(args).execute(AnswerMe.taskList, ui, storage);
-                break;
-
-            case "deadline":
-            case "event":
-                String[] segments = args.split("(?=/by|/from|/to)");
-                String desc = segments[0].trim(); // task title
-                HashMap<String, String> flags = new HashMap<>();
-                DateTimeParser dtParser = new DateTimeParser();
-
-                // skip over task title
-                for (int i = 1; i < segments.length; i++) {
-                    String segment = segments[i].trim();
-                    String[] argList = segment.split(" ", 2);
-                    if (argList[1].isBlank()) {
-                        throw new AnswerMeException("Every flag must be followed by an argument.");
-                    }
-                    flags.put(argList[0], argList[1]);
-                }
-
-                if (command.equals("deadline")) {
-                    if (!flags.containsKey("/by")) {
-                        throw new AnswerMeException("Format: <deadline> <description> /by <when>.");
-                    }
-                    LocalDateTime by = dtParser.parseDateTime(flags.get("/by"));
-                    new AddDeadlineCommand(desc, by).execute(AnswerMe.taskList, ui, storage);
-                }
-                else {
-                    if (!flags.containsKey("/from") || !flags.containsKey("/to")) {
-                        throw new AnswerMeException("Format: <event> <description> /from <when> /to <when>.");
-                    }
-                    LocalDateTime from = dtParser.parseDateTime(flags.get("/from"));
-                    LocalDateTime to = dtParser.parseDateTime(flags.get("/to"));
-                    if (!from.isBefore(to)) {
-                        throw new AnswerMeException("'From' datetime must occur before 'To'");
-                    }
-                    new AddEventCommand(desc, from, to).execute(AnswerMe.taskList, ui, storage);
-                }
-                break;
-
-            default:
-                throw new AnswerMeException("I'm not sure what you mean :(");
-        }
-    }
-
-    public static String extractArgs(String[] responseParts) {
-        String[] resized = Arrays.copyOfRange(responseParts,1,responseParts.length);
-        return String.join(" ", resized);
+    public static void main(String[] args) {
+        AnswerMe answerMeBot = new AnswerMe();
+        answerMeBot.run();
     }
 }
