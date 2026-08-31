a049793bab78e46e4c73a0a82f099e24e2376b15
RyanNgCT <ryanngct.dev@gmail.com>
2026-08-31 20:29:04 +0800

    Remove redundant function in DateTimeParser class

--- src/test/java/answerme/parser/DateTimeParserTest.java
+++ src/test/java/answerme/parser/DateTimeParserTest.java
@@ -1,5 +1,6 @@
 package answerme.parser;
 
+import answerme.exception.AnswerMeException;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -9,20 +10,20 @@ import java.time.format.DateTimeParseException;
 
 public class DateTimeParserTest {
     @Test
-    public void parseTest_validDateFormat_success() {
+    public void parseTest_validDateFormat_success() throws AnswerMeException {
         String toParse = "31/8/2026 14:00";
         LocalDateTime ldt = LocalDateTime.of(2026,8,31,14,00);
-        assertEquals(new DateTimeParser().parse(toParse), ldt);
+        assertEquals(new DateTimeParser().parseDateTime(toParse), ldt);
     }
 
     @Test
     public void parseTest_invalidDateFormat_exceptionThrown() {
         String toParse = "31/8/26 14:67";
         try {
-            new DateTimeParser().parse(toParse);
+            new DateTimeParser().parseDateTime(toParse);
             fail();
-        } catch (DateTimeParseException e) {
-            assertEquals("Invalid datetime format!", e.getMessage());
+        } catch (AnswerMeException e) {
+            assertEquals("Oh no! Ensure date/time is formatted correctly.", e.getMessage());
         }
     }
 }
