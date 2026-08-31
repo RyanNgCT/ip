1e92171766a9dbcbb48eb64c03402ef23daf1e16
RyanNgCT <ryanngct.dev@gmail.com>
2026-08-31 20:30:31 +0800

    Create additional test cases for Parser class

--- src/test/java/answerme/parser/ParserTest.java
+++ src/test/java/answerme/parser/ParserTest.java
@@ -4,11 +4,11 @@ import answerme.command.Command;
 import answerme.exception.AnswerMeException;
 import answerme.command.AddEventCommand;
 
-import java.time.LocalDateTime;
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.fail;
+
+import java.time.LocalDateTime;
 
 public class ParserTest {
     @Test
@@ -22,13 +22,33 @@ public class ParserTest {
     @Test
     public void addNewEvent_missingDescription_exceptionThrown() {
         String newEvent = "event /from 31/8/2026 17:00 /to 1/9/2026 02:00";
-        LocalDateTime d1 = LocalDateTime.of(2026,8,31,17,00);
-        LocalDateTime d2 = LocalDateTime.of(2026,9,1,02,00);
         try {
             Command eventCmd = Parser.parse(newEvent);
             fail();
         } catch (AnswerMeException e) {
             assertEquals("Oh no! Format: event <description> /from <when> /to <when>", e.getMessage());
+        }
+    }
+
+    @Test
+    public void addNewEvent_invalidDateTime_exceptionThrown() {
+        String newEvent = "event wedding dinner /from 2-09-2026 1700 /to 3-09-2026 0200";
+        try {
+            Command eventCmd = Parser.parse(newEvent);
+            fail();
+        } catch (AnswerMeException e) {
+            assertEquals("Oh no! Ensure date/time is formatted correctly.", e.getMessage());
+        }
+    }
+
+    @Test
+    public void addNewEvent_fromAfterTo_exceptionThrown() {
+        String newEvent = "event wedding dinner /from 2/9/2026 17:00 /to 1/9/2026 02:00";
+        try {
+            Command eventCmd = Parser.parse(newEvent);
+            fail();
+        } catch (AnswerMeException e) {
+            assertEquals("Oh no! 'From' datetime must occur before 'To'.", e.getMessage());
         }
     }
 }
