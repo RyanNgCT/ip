85aff6d5e4e3ffff4b80ff325e7a50d5788b398c
RyanNgCT <ryanngct.dev@gmail.com>
2026-09-01 15:24:15 +0800

    Add JavaDoc for Task and subclasses

--- src/main/java/answerme/task/Task.java
+++ src/main/java/answerme/task/Task.java
@@ -1,19 +1,40 @@
 package answerme.task;
 
+/**
+ * Represents a task that can be marked as complete or incomplete.
+ */
 public class Task {
     protected String description;
     protected boolean isComplete;
 
+    /**
+     * Constructs a new task with a description specified. Its default status
+     * is incomplete (i.e. {@code false}).
+     *
+     * @param description The task description.
+     */
     public Task(String description) {
         this.description = description;
         this.isComplete = false;
     }
 
+    /**
+     * Creates a task with the specified description and completion status.
+     *
+     * @param description The task description.
+     * @param isComplete The completion status of the task.
+     */
     public Task(String description, boolean isComplete) {
         this.description = description;
         this.isComplete = isComplete;
     }
 
+    /**
+     * Returns the icon representing the current task's completion status.
+     *
+     * @return {@code "X"} if the task is complete, otherwise a
+     *                     {@code " "} character.
+     */
     public String getStatusIcon() {
         return (getCompletedStatus() ? "X" : " ");
     }
@@ -39,16 +60,30 @@ public class Task {
         return this.isComplete;
     }
 
+    /**
+     * Returns this task's status icon followed by its description.
+     */
     @Override
     public String toString() {
         return "[" + getStatusIcon() + "] " + description;
     }
 
+    /**
+     * Formats the (sub)task into its storage format.
+     *
+     * @param type The sub-task type.
+     * @return The formatted task.
+     */
     protected String getLogFormat(String type) {
         String status = isComplete ? "Complete" : "Incomplete";
         return type + " | " + status + " | " + description;
     }
 
+    /**
+     * Returns this task's data in the format used for storage.
+     *
+     * @return The storage representation of this task.
+     */
     public String toStorageFormat() {
         return "Task";
     }
--- src/main/java/answerme/task/TaskList.java
+++ src/main/java/answerme/task/TaskList.java
@@ -4,15 +4,33 @@ import answerme.exception.AnswerMeException;
 import java.util.ArrayList;
 import java.util.Collection;
 
+/**
+ * Represents a list of tasks.
+ */
 public class TaskList extends ArrayList<Task>{
+    /**
+     * Constructs an empty task list.
+     */
     public TaskList() {
         super();
     }
 
+    /**
+     * Constructs a task list from a collection of tasks.
+     *
+     * @param collection The collection of tasks to be added.
+     */
     public TaskList(Collection<Task> collection) {
         super(collection);
     }
 
+    /**
+     * Retrieves the specified task based on its index.
+     *
+     * @param taskIndex The index of the task to return.
+     * @return The target task.
+     * @throws AnswerMeException If the list is empty or the index is invalid.
+     */
     public Task getTask(int taskIndex)
             throws AnswerMeException {
         if (this.size() == 0) {
@@ -25,6 +43,11 @@ public class TaskList extends ArrayList<Task>{
         return this.get(taskIndex);
     }
 
+    /**
+     * {@inheritDoc}
+     *
+     * Returns the tasks as a printable numbered list.
+     */
     @Override
     public String toString() {
         String result = "";
